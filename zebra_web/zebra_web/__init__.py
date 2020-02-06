caab40c73df5ed9ba0102a83f2f5dc8c318f10be
Fatima Nadeem <fatima.nadeem@mail.utoronto.ca>
2020-02-06 18:14:55 -0500

    Added sign-in page - not signing in people atm

--- zebra_web/zebra_web/__init__.py
+++ zebra_web/zebra_web/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def main():
+@app.route("/start")
+def start():
     return render_template('index.html')
+
+@app.route('/signIn')
+def signIn():
+    return render_template('signIn.html')
+
 
 if __name__ == "__main__":
     app.run()
