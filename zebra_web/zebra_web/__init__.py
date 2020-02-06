b65c6dcc58b7edf0e46b7da23594f3ac0eee98c6
Fatima Nadeem <fatima.nadeem@mail.utoronto.ca>
2020-02-06 18:40:45 -0500

    added main page - not connected other pages atm

--- zebra_web/zebra_web/__init__.py
+++ zebra_web/zebra_web/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/start")
+@app.route("/")
 def start():
     return render_template('index.html')
 
 @app.route('/signIn')
 def signIn():
     return render_template('signIn.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 
 if __name__ == "__main__":
