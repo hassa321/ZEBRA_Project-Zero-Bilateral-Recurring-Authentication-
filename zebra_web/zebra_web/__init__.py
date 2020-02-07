9b644f2a90087c7c16426f24b940b877a40e1331
Fatima Nadeem <fatima.nadeem@mail.utoronto.ca>
2020-02-07 09:45:50 -0500

    Fixed redirection after login

--- zebra_web/zebra_web/__init__.py
+++ zebra_web/zebra_web/__init__.py
@@ -1,28 +1,23 @@
-from flask import Flask, render_template, request, json, flash
+from flask import Flask, render_template, request, json, flash, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/showLogin')
-def showLogin():
-    return render_template('login.html')
-
-@app.route('/main')
-def main():
-    return render_template('main.html')
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        print ("fdgd")
-        if request.form['inputUserName'] != 'admin' or request.form['inputPassword'] != 'password':
+        if request.form['inputUserName'] != 'admin' or request.form['inputPassword'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == "__main__":
     app.run()
