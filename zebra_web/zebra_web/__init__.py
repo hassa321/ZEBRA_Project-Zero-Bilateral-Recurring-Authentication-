57e2641f0d11271fc028c02a89b60519387f4437
Fatima Nadeem <fatima.nadeem@mail.utoronto.ca>
2020-02-06 20:39:12 -0500

    login page partly works - not moving to next pg

--- zebra_web/zebra_web/__init__.py
+++ zebra_web/zebra_web/__init__.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 @app.route("/")
-def start():
+def index():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
+@app.route('/showLogin')
+def showLogin():
     return render_template('login.html')
 
 @app.route('/main')
 def main():
     return render_template('main.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    _userName = request.form['inputUserName']
+    _password = request.form['inputPassword']
+ 
+    if _userName == 'admin' and _password == 'password':
+        # return main()
+        return json.dumps({'html':'<span>Credentials match !!</span>'})
+    else:
+        # flash("wrong password!!")
+        return json.dumps({'html':'<span>Wrong credentials</span>'})
 
 if __name__ == "__main__":
     app.run()
