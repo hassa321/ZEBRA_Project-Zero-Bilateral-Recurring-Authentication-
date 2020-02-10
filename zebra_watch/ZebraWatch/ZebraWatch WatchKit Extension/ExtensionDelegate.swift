62c8b788d34a91aa0b821e617bea4ee09f33e1a7
vladislawa <34406441+vladislawa@users.noreply.github.com>
2020-02-09 19:13:10 -0500

    Printing data from the watch

---
+++ zebra_watch/ZebraWatch/ZebraWatch WatchKit Extension/ExtensionDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  ExtensionDelegate.swift
+//  ZebraWatch WatchKit Extension
+//
+//  Created by Vladyslava Diachenko on 2020-02-01.
+//  Copyright © 2020 Vladyslava Diachenko. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {}
