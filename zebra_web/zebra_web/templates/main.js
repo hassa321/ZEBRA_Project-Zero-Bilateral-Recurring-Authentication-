b8cff77ac048d887a0e287508b522b88b487368d
USER <user@owner-2.local>
2020-03-31 04:10:43 -0400

    .

---
+++ zebra_web/zebra_web/templates/main.js
@@ -0,0 +1,8 @@
+var typingTimer;
+var doneTypingInterval = 5000;
+
+const textarea = document.getElementById("textarea")
+textarea.addEventListener("click", function(){
+  document.getElementById("demo").innerHTML = "Hello World";
+});
+
